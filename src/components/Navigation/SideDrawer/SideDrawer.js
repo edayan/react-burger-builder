270f23168eb7742452c63324deeac3e2ca338067
Saju Paul <saju.christel@blu-pa.com>
2019-12-21 20:45:28 +0530

    changing the folder structure to put statefull component into containers

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../hoc/AuxHoc';
+import Aux from '../../../hoc/AuxHoc/AuxHoc';
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationsItems';
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
-import Aux from '../../../hoc/AuxHoc';
+import Aux from '../../../hoc/AuxHoc/AuxHoc';
 
 
 class Modal extends Component {
