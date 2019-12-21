a626013d6285208e9081e5be54c014317aa4d3ec
Saju Paul <saju.christel@blu-pa.com>
2019-12-21 20:08:53 +0530

    Fixing side drawer open close on mobile

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,22 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import Aux from '../../../hoc/AuxHoc';
 import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationsItems';
-import classes from './SideDrawer.module.css'
+import classes from './SideDrawer.module.css';
+
 
 const SideDrawer = (props) => {
-
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
     return (
-        <div className={classes.SideDrawer}>
-            {/* <Logo height="11%" /> */}
-            <div className={classes.Logo}>
-                <Logo />
-            </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                {/* <Logo height="11%" /> */}
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
 
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+
     )
 }
 
