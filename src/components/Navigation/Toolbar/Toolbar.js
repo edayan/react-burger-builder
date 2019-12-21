b3253f4271cf3ea6865396533258d336a952e95f
Saju Paul <saju.christel@blu-pa.com>
2019-12-21 19:18:07 +0530

    Implementing navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.css';
+import NavigationItems from '../NavigationItems/NavigationsItems';
 
 const toolbar = () => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
             <Logo />
-            <nav>...</nav>
+            <nav><NavigationItems /></nav>
         </header>
     )
 }
