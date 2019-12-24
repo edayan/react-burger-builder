32dd16351f33e60d646249625dc2dccb6653ba58
Saju Paul <saju.christel@blu-pa.com>
2019-12-24 11:46:02 +0530

    Adding error handler higher order component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../../hoc/AuxHoc/AuxHoc';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -149,4 +150,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import AuxHoc from '../AuxHoc/AuxHoc';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null }); //clear the error on request
+        return req;
+      });
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <AuxHoc>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </AuxHoc>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
