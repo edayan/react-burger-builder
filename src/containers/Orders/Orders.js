dbec3279d36c37999af7a33b6b4784b7ed443492
Saju Paul <saju.christel@blu-pa.com>
2019-12-28 15:47:25 +0530

    Fetching the orders from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import Order from '../../components/UI/Order/Order';
-
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    axios
+      .get('/orders.json')
+      .then(result => {
+        const fetachedOrders = [];
+        for (let key in result.data) {
+          fetachedOrders.push({
+            ...result.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetachedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
-    return (
-      <div>
-        <Order />
-      </div>
-    );
+    let order = this.state.orders.map(order => {
+      return <Order key={order.id} />;
+    });
+    return <div>{order}</div>;
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
