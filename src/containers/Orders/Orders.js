3b0c2fc3339e9723ef75f37d0e28ce97ed43fcbd
Saju Paul <saju.christel@blu-pa.com>
2019-12-28 15:22:50 +0530

    Adding hard coded orders and route

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Order from '../../components/UI/Order/Order';
+
+class Orders extends Component {
+  render() {
+    return (
+      <div>
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default Orders;
