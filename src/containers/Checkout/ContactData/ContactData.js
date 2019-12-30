31a6c04c57cb4ae842a726c674e5aeb3de331ed3
Saju Paul <saju.christel@blu-pa.com>
2019-12-30 15:30:17 +0530

    validate only if form is touched

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -20,7 +20,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -32,7 +33,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -46,7 +48,8 @@ class ContactData extends Component {
           minLength: 4,
           maxLength: 5
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -58,7 +61,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -70,7 +74,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -142,6 +147,7 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation
     );
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({ orderForm: updatedOrderForm });
   };
@@ -163,6 +169,7 @@ class ContactData extends Component {
               invalid={!formElement.config.valid}
               shouldValidate={formElement.config.validation}
               changed={event => this.inputChangedHandler(event, formElement.id)}
+              touched={formElement.config.touched}
             />
           );
         })}
