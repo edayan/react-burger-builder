dae7d2bf2d777f0aebaeba68c68dad30a8c89291
Saju Paul <saju.christel@blu-pa.com>
2020-01-02 22:07:50 +0530

    installing redux and configuring store

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTYpes from './actions';
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  }
+  return state;
+};
+
+export default reducer;
