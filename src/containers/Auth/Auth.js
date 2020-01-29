0bace5ca3f3afa7fbf6c7427fe047b4f06604aa7
Saju Paul <saju.christel@blu-pa.com>
2020-01-29 17:07:05 +0100

    Addinng form submission and action types for logging in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import { connect } from 'react-redux';
+
+import * as actions from '../../store/actions/index';
+
 import classes from './Auth.module.css';
 
 class Auth extends Component {
@@ -73,6 +77,14 @@ class Auth extends Component {
     return isValid;
   }
 
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -92,7 +104,7 @@ class Auth extends Component {
     ));
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -100,5 +112,9 @@ class Auth extends Component {
     );
   }
 }
-
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth())
+  };
+};
+export default connect(null, mapDispatchToProps)(Auth);
