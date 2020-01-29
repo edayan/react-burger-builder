83824a32961acc2d9c893f7e7dee9907964d2bd7
Saju Paul <saju.christel@blu-pa.com>
2020-01-29 16:31:47 +0100

    Adding validations

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -38,6 +38,41 @@ class Auth extends Component {
     }
   };
 
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true
+      }
+    };
+    this.setState({ controls: updatedControls });
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules) {
+      if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+      }
+
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
+
+      if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+      }
+    }
+
+    return isValid;
+  }
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
