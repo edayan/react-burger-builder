1bed13e32cf423e23c39009eb7669a90ac783bab
Saju Paul <saju.christel@blu-pa.com>
2020-01-30 20:33:47 +0100

    signing up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,31 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      token: '',
+      returnSecureToken: true
+    };
+
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyC9I_sV0m_Cg2yZxcAKrC5I17QrUmCPZZc';
+    // let url='https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC9I_sV0m_Cg2yZxcAKrC5I17QrUmCPZZc'
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        const data = {
+          email: 'test@test.com',
+          expiresIn: '3600',
+          idToken: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`
+        };
+        dispatch(authSuccess(data));
+        // console.log(err);
+        // dispatch(authFail(err));
+      });
   };
 };
