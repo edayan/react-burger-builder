5f5e2c27db453cba16b11088c2afe10d5dff9ad8
Saju Paul <saju.christel@blu-pa.com>
2020-02-21 14:42:13 +0100

    implementing logout and redirection to home page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" exact component={Orders} />
-            <Route path="/auth" exact component={Auth} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Layout>
+					<Switch>
+						<Route path="/checkout" component={Checkout} />
+						<Route path="/orders" exact component={Orders} />
+						<Route path="/auth" exact component={Auth} />
+						<Route path="/logout" exact component={Logout} />
+						<Route path="/" exact component={BurgerBuilder} />
+					</Switch>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
