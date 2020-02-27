b7501e4bb4df4b47a14179f78b45d33f1decc5d4
Saju Paul <saju.christel@blu-pa.com>
2020-02-27 12:00:27 +0100

    Protecting routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -15,25 +15,41 @@ class App extends Component {
 	}
 
 	render() {
+		let routes = (
+			<Switch>
+				<Route path="/auth" exact component={Auth} />
+				<Route path="/" exact component={BurgerBuilder} />
+				<Redirect to="/" />
+			</Switch>
+		);
+
+		if (this.props.isAuthenticated) {
+			routes = (
+				<Switch>
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/orders" exact component={Orders} />
+					<Route path="/logout" exact component={Logout} />
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</Switch>
+			);
+		}
 		return (
 			<div>
-				<Layout>
-					<Switch>
-						<Route path="/checkout" component={Checkout} />
-						<Route path="/orders" exact component={Orders} />
-						<Route path="/auth" exact component={Auth} />
-						<Route path="/logout" exact component={Logout} />
-						<Route path="/" exact component={BurgerBuilder} />
-					</Switch>
-				</Layout>
+				<Layout>{routes}</Layout>
 			</div>
 		);
 	}
 }
 
+const mapSateToProps = state => {
+	return {
+		isAuthenticated: state.auth.token !== null
+	};
+};
 const mapDispatchToProps = dispatch => {
 	return {
 		onTryAutoSignUp: () => dispatch(actions.authCheckState())
 	};
 };
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapSateToProps, mapDispatchToProps)(App));
