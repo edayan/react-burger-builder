905875c21a25f1ef6f8cf8e5f602f7c55b12471c
Saju Paul <saju.christel@blu-pa.com>
2020-04-05 11:24:12 +0530

    extracting utility methods to remove duplication

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions/index';
 
 import classes from './Auth.module.css';
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 class Auth extends Component {
 	state = {
@@ -57,7 +57,7 @@ class Auth extends Component {
 		const updatedControls = updateObject(this.state.controls, {
 			[controlName]: updateObject(this.state.controls[controlName], {
 				value: event.target.value,
-				valid: this.checkValidity(
+				valid: checkValidity(
 					event.target.value,
 					this.state.controls[controlName].validation
 				),
@@ -66,25 +66,6 @@ class Auth extends Component {
 		});
 		this.setState({ controls: updatedControls });
 	};
-
-	checkValidity(value, rules) {
-		let isValid = true;
-		if (rules) {
-			if (rules.required) {
-				isValid = value.trim() !== '' && isValid;
-			}
-
-			if (rules.minLength) {
-				isValid = value.length >= rules.minLength && isValid;
-			}
-
-			if (rules.maxLength) {
-				isValid = value.length <= rules.maxLength && isValid;
-			}
-		}
-
-		return isValid;
-	}
 
 	submitHandler = event => {
 		event.preventDefault();
