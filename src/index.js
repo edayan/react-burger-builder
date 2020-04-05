45aa2df857266a6b04d70a72f2b400ad096e2b4a
Saju Paul <saju.christel@blu-pa.com>
2020-04-05 13:15:30 +0530

    Fixing start issue after ejecting

--- src/index.js
+++ src/index.js
@@ -13,26 +13,27 @@ import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
-  burgerBuilder: burgerBuilderReducer,
-  order: orderReducer,
-  auth: authReducer
+	burgerBuilder: burgerBuilderReducer,
+	order: orderReducer,
+	auth: authReducer
 });
 
 const composeEnhancers =
-  (typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+	process.env.NODE_ENV === 'development'
+		? typeof window !== 'undefined' &&
+		  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
 );
 
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
